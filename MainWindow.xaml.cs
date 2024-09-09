0c7f64153c8e06db3bdba7bda36250624f0515ec
stigibalint <72031994+stigibalint@users.noreply.github.com>
2024-09-09 13:03:30 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,22 @@
-﻿using LEGO;
-using Microsoft.Win32;
-using System;
+﻿using Microsoft.Win32;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 using System.Xml.Linq;
 
-namespace LEGO
+namespace WpfApp2
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public ObservableCollection<LegoItem> LegoItems { get; set; } = new ObservableCollection<LegoItem>();
@@ -18,7 +25,7 @@ namespace LEGO
         {
             InitializeComponent();
             DataContext = this;
-            dataGrid.ItemsSource = CollectionViewSource.GetDefaultView(LegoItems);
+            dataGrid.ItemsSource = LegoItems; // Itt cseréltem le a CollectionViewSource-t
         }
 
         private void LoadData(string filePath)
@@ -49,7 +56,6 @@ namespace LEGO
                 MessageBox.Show($"Error loading file: {ex.Message}");
             }
         }
-
         private void OpenFileButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -59,24 +65,25 @@ namespace LEGO
                 LoadData(openFileDialog.FileName);
             }
         }
-    
 
 
-private void ApplyFilters(string categoryName)
+
+        private void ApplyFilters(string categoryName, string itemName)
         {
             var view = CollectionViewSource.GetDefaultView(LegoItems);
             view.Filter = item =>
             {
                 var legoItem = item as LegoItem;
-                bool categoryMatch = string.IsNullOrEmpty(categoryName) || legoItem.CategoryName == categoryName;
-              
-                return categoryMatch;
+                bool categoryMatch = string.IsNullOrEmpty(categoryName) || legoItem.CategoryName.ToLower().Contains(categoryName.ToLower());
+                bool itemNameMatch = string.IsNullOrEmpty(itemName) || legoItem.ItemName.ToLower().Contains(itemName.ToLower());
+
+
+                return categoryMatch && itemNameMatch;
             };
         }
-
         private void OnFilterChanged(object sender, TextChangedEventArgs e)
         {
-            ApplyFilters(categoryFilterTextBox.Text);
+            ApplyFilters(categoryFilterTextBox.Text, itemFilterTextBox.Text);
         }
     }
 }
